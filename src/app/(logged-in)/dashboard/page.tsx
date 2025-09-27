d49cd0c1f8720b877cc9fbff7fc62070fbf16801
04widodo <04widodo@gmail.com>
2025-09-27 17:10:18 +0700

    tambah user

--- src/app/(logged-in)/dashboard/page.tsx
+++ src/app/(logged-in)/dashboard/page.tsx
@@ -3,9 +3,11 @@
 import LoadingStore from "@/store/loadingStore";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
+import { Popover } from "react-tiny-popover";
 
 export default function Dashboard() {
   const setLoading = LoadingStore((state) => state.setLoading);
+  const [showPopover, setShowPopover] = useState<boolean>(false);
 
   const [data, setData] = useState<any[]>([
     { nama: "Matematika", kelas: "11", guru: "Pak Guru A", modulSelesai: 9, totalModul: 9 },
@@ -44,9 +46,25 @@ export default function Dashboard() {
           </div>
         </div>
       ))}
-      <div className="rounded-full w-20 h-20 flex items-center justify-center bg-primary absolute right-10 bottom-10 cursor-pointer">
-        <PlusIcon className="w-10 h-10 text-white" />
-      </div>
+      <Popover
+        isOpen={showPopover}
+        onClickOutside={() => setShowPopover(false)}
+        positions="top"
+        align="end"
+        content={
+          <div className="flex flex-col gap-2 bg-white rounded-lg p-4 border-gray-300 border font-semibold mb-4">
+            <div className="cursor-pointer hover:bg-gray-300 px-2 rounded-sm">User</div>
+            <div className="cursor-pointer hover:bg-gray-300 px-2 rounded-sm">Mata Pelajaran</div>
+          </div>
+        }
+      >
+        <div
+          className="rounded-full w-20 h-20 flex items-center justify-center bg-primary absolute right-10 bottom-10 cursor-pointer"
+          onClick={() => setShowPopover(!showPopover)}
+        >
+          <PlusIcon className="w-10 h-10 text-white" />
+        </div>
+      </Popover>
     </div>
   );
 }
